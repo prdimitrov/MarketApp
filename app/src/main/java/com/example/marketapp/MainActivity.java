25a6b9696e9e2613b437cbed7956e3ddf2badda8
prdimitrov <prdimitrov97@gmail.com>
2024-12-03 00:18:17 +0200

    Finish the adapter methods

--- app/src/main/java/com/example/marketapp/MainActivity.java
+++ app/src/main/java/com/example/marketapp/MainActivity.java
@@ -7,8 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.marketapp.adapter.MyAdapter;
+import com.example.marketapp.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 1 - AdapterView
+    RecyclerView recyclerView;
+    // 2 - Data Source
+    List<Item> itemList;
+    // 3 - Adapter
+    MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,21 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            recyclerView = findViewById(R.id.recyclerView);
+
+            itemList = new ArrayList<>();
+            Item item1 = new Item(R.drawable.fruit, "Fruits", "Fresh Fruits from the Garden");
+            itemList.add(item1);
+            itemList.add(new Item(R.drawable.vegitables, "Vegetables", "Delicious Vegetables"));
+            itemList.add(new Item(R.drawable.bread, "Bakery", "Bread, Wheat and Beans"));
+            itemList.add(new Item(R.drawable.beverage, "Beverage", "Juice, Tea, Coffee and Soda"));
+            itemList.add(new Item(R.drawable.milk, "Milk", "Milk, Shakes and Yogurt"));
+            itemList.add(new Item(R.drawable.popcorn, "Snacks", "Pop Corn, Donut and Drinks"));
+
+            myAdapter = new MyAdapter(itemList);
+            recyclerView.setAdapter(myAdapter);
+
             return insets;
         });
     }
--- app/src/main/java/com/example/marketapp/adapter/MyAdapter.java
+++ app/src/main/java/com/example/marketapp/adapter/MyAdapter.java
@@ -3,6 +3,8 @@ package com.example.marketapp.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,6 +27,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // responsible for creating new view holders for your items
 
+
         View itemView = LayoutInflater
                 .from(parent.getContext())
                 .inflate(R.layout.item_layout, parent, false);
@@ -36,20 +39,30 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         // binds the data from your dataset to the views within the view holder
+        Item item = itemList.get(position);
+
+        holder.title.setText(item.getItemName());
+        holder.description.setText(item.getItemDescription());
+        holder.imageView.setImageResource(item.getItemImage());
 
     }
 
     @Override
     public int getItemCount() {
         // returns the total number of items in your dataset
-
-        return 0;
+        return itemList.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-
+        // This inner class holds references to the views within the item layout
+        ImageView imageView;
+        TextView title, description;
         public MyViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            imageView = itemView.findViewById(R.id.imageview);
+            title = itemView.findViewById(R.id.title_text);
+            description = itemView.findViewById(R.id.description_text);
         }
     }
 }
