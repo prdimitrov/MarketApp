6b8dbdf2d4926a37b85990e6862a42e4034f115f
prdimitrov <prdimitrov97@gmail.com>
2024-12-02 23:56:21 +0200

    Create custom adapter

--- app/src/main/java/com/example/marketapp/adapter/MyAdapter.java
+++ app/src/main/java/com/example/marketapp/adapter/MyAdapter.java
@@ -3,8 +3,6 @@ package com.example.marketapp.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,7 +25,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // responsible for creating new view holders for your items
 
-
         View itemView = LayoutInflater
                 .from(parent.getContext())
                 .inflate(R.layout.item_layout, parent, false);
@@ -39,30 +36,20 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         // binds the data from your dataset to the views within the view holder
-        Item item = itemList.get(position);
-
-        holder.title.setText(item.getItemName());
-        holder.description.setText(item.getItemDescription());
-        holder.imageView.setImageResource(item.getItemImage());
 
     }
 
     @Override
     public int getItemCount() {
         // returns the total number of items in your dataset
-        return itemList.size();
+
+        return 0;
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // This inner class holds references to the views within the item layout
-        ImageView imageView;
-        TextView title, description;
-        public MyViewHolder(@NonNull View itemView) {
 
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            imageView = itemView.findViewById(R.id.imageview);
-            title = itemView.findViewById(R.id.title_text);
-            description = itemView.findViewById(R.id.description_text);
         }
     }
 }
