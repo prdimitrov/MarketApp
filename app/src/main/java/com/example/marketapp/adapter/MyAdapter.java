6b61dc07e8ce75db22b6e768c01b65a9dddbf2af
prdimitrov <prdimitrov97@gmail.com>
2024-12-03 21:12:59 +0200

    Handle click events on RecyclerView items -Add ItemClickListener interface

--- app/src/main/java/com/example/marketapp/adapter/MyAdapter.java
+++ app/src/main/java/com/example/marketapp/adapter/MyAdapter.java
@@ -3,11 +3,14 @@ package com.example.marketapp.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.marketapp.R;
+import com.example.marketapp.listener.ItemClickListener;
 import com.example.marketapp.model.Item;
 
 import java.util.List;
@@ -15,6 +18,12 @@ import java.util.List;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     private List<Item> itemList;
+
+    public ItemClickListener clickListener;
+
+    public void setClickListener(ItemClickListener myListener) {
+        this.clickListener = myListener;
+    }
 
     public MyAdapter(List<Item> itemList) {
         this.itemList = itemList;
@@ -46,10 +55,25 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return 0;
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-
+    public class MyViewHolder extends RecyclerView.ViewHolder
+    implements View.OnClickListener {
+        ImageView imageView;
+        TextView title;
+        TextView description;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.imageview);
+            title = itemView.findViewById(R.id.title_text);
+            description = itemView.findViewById(R.id.description_text);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (clickListener != null) {
+                clickListener.onClick(v, getAdapterPosition());
+            }
         }
     }
 }
